058de9dd53353f9007ae6ea1bc2ac3078fbef80a
Manickam P <pmanickam81@gmail.com>
2019-08-05 13:43:08 +0530

    GraphQL updated

---
+++ sample.js
@@ -0,0 +1,13 @@
+// for (i = 0; i < 3; i++) {
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 3000)
+// }
+
+
+const user = { name: "Viji" }
+
+user.name = "murugan"
+
+
+console.log(user)
